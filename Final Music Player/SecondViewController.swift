8bcb823268d570b7123b94f46a359952bec14d72
Xiaoyu Pan <xiaoyup1999@gmail.com>
2019-09-27 00:39:06 +1200

    Initial Commit

---
+++ Final Music Player/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Final Music Player
+//
+//  Created by Rongyu on 27/09/19.
+//  Copyright © 2019 Xiaoyu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Final Music PlayerTests/Final_Music_PlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Final_Music_PlayerTests.swift
+//  Final Music PlayerTests
+//
+//  Created by Rongyu on 27/09/19.
+//  Copyright © 2019 Xiaoyu. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Music_Player
+
+class Final_Music_PlayerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
