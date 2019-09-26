b6a9f22fae65796e81e90ee2cbbfb53cce5275ad
Xiaoyu Pan <xiaoyup1999@gmail.com>
2019-09-27 03:49:40 +1200

    next song

--- Final Music Player/SecondViewController.swift
+++ Final Music Player/SecondViewController.swift
@@ -7,20 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SecondViewController: UIViewController {
 
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var label: UILabel!
-    @IBAction func Play(_ sender: Any) {
+    @IBAction func Play(_ sender: Any)
+    {
+        if audioPlayer.isPlaying == false
+        {
+            audioPlayer.play()
+        }
     }
-    @IBAction func Pause(_ sender: Any) {
+    @IBAction func Pause(_ sender: Any)
+    {
+        if audioPlayer.isPlaying
+        {
+            audioPlayer.pause()
+        }
     }
-    @IBAction func Prev(_ sender: Any) {
+    @IBAction func Prev(_ sender: Any)
+    {
+        
     }
-    @IBAction func next(_ sender: Any) {
+    @IBAction func next(_ sender: Any)
+    {
+        playThis(thisThing: songs[thisSong+1])
     }
-    @IBAction func UiSlider(_ sender: Any) {
+    @IBAction func UiSlider(_ sender: Any)
+    {
+        audioPlayer.volume = (sender as AnyObject).value
+    }
+    func playThis(thisThing:String)
+    {
+    do{
+        let audioPath = Bundle.main.path(forResource: thisThing, ofType: ".mp3")
+        try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!)as URL)
+        thisSong += 1
+        audioPlayer.play()
+      
+    }
+    catch{
+        print ("ERROR")
+    }
+  
     }
     override func viewDidLoad() {
         super.viewDidLoad()
