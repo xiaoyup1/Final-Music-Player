122b046c5904b825bd349442ae82996c1dfd0021
Xiaoyu Pan <xiaoyup1999@gmail.com>
2019-09-27 02:50:28 +1200

    songs can played

--- Final Music Player/FirstViewController.swift
+++ Final Music Player/FirstViewController.swift
@@ -6,39 +6,44 @@
 //  Copyright © 2019 Xiaoyu. All rights reserved.
 //
 
+
+
 import UIKit
 import AVFoundation
 
 
-var songs:[String]=[]
 var audioPlayer = AVAudioPlayer()
+var songs:[String] = []
+var thisSong = 0
+var audioStuffed = false
 
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var myTableView: UITableView!
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return songs.count
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
         cell.textLabel?.text = songs[indexPath.row]
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
-        do
-        {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        do{
             let audioPath = Bundle.main.path(forResource: songs[indexPath.row], ofType: ".mp3")
             try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!)as URL)
             audioPlayer.play()
+            thisSong = indexPath.row
+            audioStuffed = true
         }
-        catch
-        {
-            print("Error")
+        catch{
+            print ("ERROR")
         }
     }
 
@@ -48,36 +53,35 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         gettingSongName()
     }
-    func gettingSongName()
-    {
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func gettingSongName() {
         let folderURL = URL(fileURLWithPath: Bundle.main.resourcePath!)
         
-        do
-        {
+        do {
             let songPath = try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
             
-            for song in songPath
-            {
-                var mySong  = song.absoluteString
+            for song in songPath {
+                var mySong = song.absoluteString
                 
-                if mySong.contains(".mp3")
-                {
+                if mySong.contains(".mp3") {
                     let findString = mySong.components(separatedBy: "/")
-                    mySong = (findString[findString.count-1])
+                    mySong =  findString[findString.count-1]
                     mySong = mySong.replacingOccurrences(of: "%20", with: " ")
-                    mySong = mySong.replacingOccurrences(of: ".mp3", with: " ")
-                    mySong = mySong.replacingOccurrences(of: "%5B", with: " ")
-                    mySong = mySong.replacingOccurrences(of: "%5D", with: " ")
+                    mySong = mySong.replacingOccurrences(of: ".mp3", with: "")
                     songs.append(mySong)
                 }
             }
             myTableView.reloadData()
         }
-        catch
-        {
+        catch {
             
         }
     }
+
 
 }
 
