b19abccceda14d1087a25694ca52e0db5bf3ff7f
Xiaoyu Pan <xiaoyup1999@gmail.com>
2019-09-27 03:06:18 +1200

    Screen 2

--- Final Music Player/SecondViewController.swift
+++ Final Music Player/SecondViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func Play(_ sender: Any) {
+    }
+    @IBAction func Pause(_ sender: Any) {
+    }
+    @IBAction func Prev(_ sender: Any) {
+    }
+    @IBAction func next(_ sender: Any) {
+    }
+    @IBAction func UiSlider(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
